d2a8f51cc3b7ddcdaf1b17837fce96a4a833832e
LogicalJoe <LJ@LogicalJoe.com>
2024-11-18 21:09:37 -0500

    Add TI-80 checksum, tidy checksums

--- TI ROM checksums.tcl
+++ TI ROM checksums.tcl
@@ -1,22 +1,40 @@
 # TI-ROM verification HexFiend template
-# Version 1.0
-# (c) 2021-2023 LogicalJoe
+# Version 2.0
+# (c) 2021-2024 LogicalJoe
 
 set hl 0
-set hl2 0
-set a 0
-
-while {[incr a]<[len]-1} {
-	# get the next byte
-	set c [uint8]
-	#1.6K+
-	incr hl2 $c
-	#1.5K-
-	set hl [expr ((4129*((($hl>>12)^($hl>>8))&15))^(528*($hl>>12))^($hl<<8)^$c)&65535]
+switch -- [len] {
+	32768 { # TI-81
+		set hl2 0
+		while {[pos]<[len]-2} {
+			set c [uint8]
+			#1.6K+
+			incr hl2 $c
+			#1.5K- (CRC16-CCITT)
+			set hl [expr ((4129*((($hl>>12)^($hl>>8))&15))^(528*($hl>>12))^($hl<<8)^$c)&65535]
+		}
+		entry "Hash 1.5K-:" [format %04X $hl]
+		entry "Hash 1.6K+:" [format %04X [expr 65535&-$hl2]]
+		entry "Current:" [format %04X [uint16]] 2 [expr [len]-2]
+	}
+	49152 { # TI-80
+		# yes, only part of the ROM
+		goto 0x4000
+		while {[pos]<45055} {
+			incr hl [uint16]
+		}
+		entry "Checksum:" [format %02X [expr 65535&$hl]]
+		goto 49150
+		entry "Current:" [format %04X [uint16]] 2 [expr [len]-2]
+	}
+	131072 - 262144 { # TI-8[2356]
+		while {![end]} {
+			incr hl [uint8]
+		}
+		entry "Checksum:" [format %02X [expr 255&$hl]]
+		# TI-82 19.006 doubles the checksum so the valid ROM returns 0x80
+	}
+	default {
+		entry "Cannot apply" ""
+	}
 }
-entry "TI-81 hash 1.5K-:" [format %04X $hl]
-entry "TI-81 hash 1.6K+:" [format %04X [expr 65535&-$hl2]]
-entry "TI-81 hash curnt:" [format %04X [uint16]] 2 [expr [len]-2]
-move -2
-entry "Z80 checksum:" [format %02X [expr 255&($hl2+[uint8]+[uint8])]]
-# TI-82 19.006 doubles the checksum so valid ROMs return 0x80
